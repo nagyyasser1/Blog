4de82d49c0bc5e32fc95a32798e8616854515ddd
nagy <nagyy8751@gmail.com>
2023-03-10 17:27:13 +0200

    fix image

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,19 +1,3 @@
-// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAeVtaRz_l_hJ5-PCqw2FEzxqpWVe35bkA',
-//   authDomain: 'blogging-website-2f62d.firebaseapp.com',
-//   projectId: 'blogging-website-2f62d',
-//   storageBucket: 'blogging-website-2f62d.appspot.com',
-//   messagingSenderId: '66030395933',
-//   appId: '1:66030395933:web:3e603b65d4012040751cf4',
-// }
-
-// Initialize Firebase
-// firebase.initializeApp(firebaseConfig)
-
-// let db = firebase.firestore()
-// --------------------------------------------------------------====
 import { db } from '../js/firebase.js'
 const blogTitleField = document.querySelector('.title')
 const articleFeild = document.querySelector('.article')
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const path = require('path')
-const fileupload = require('express-fileupload')
-
-let initial_path = path.join(__dirname, 'public')
+// const fileupload = require('express-fileupload')
+const multer = require('multer')
 
 const app = express()
+
+let initial_path = path.join(__dirname, 'public')
 app.use(express.static(initial_path))
-app.use(fileupload())
+// app.use(fileupload())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(initial_path, 'home.html'))
@@ -16,24 +17,28 @@ app.get('/editor', (req, res) => {
   res.sendFile(path.join(initial_path, 'editor.html'))
 })
 
-// upload link
-app.post('/upload', (req, res) => {
-  let file = req.files.image
-  let date = new Date()
-  // image name
-  let imagename = date.getDate() + date.getTime() + file.name
-  // image upload path
-  let path = 'public/uploads/' + imagename
+// --------image store configration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, initial_path)
+  },
+  filename: function (req, file, cb) {
+    imagename = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    file_path =
+      'uploads/' + file.fieldname + '-' + imagename + file.originalname
+    cb(null, file_path)
+  },
+})
 
-  // create upload
-  file.mv(path, (err, result) => {
-    if (err) {
-      throw err
-    } else {
-      // our image upload path
-      res.json(`uploads/${imagename}`)
-    }
-  })
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+})
+// upload link
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json(`${file_path}`)
 })
 
 app.get('/:blog', (req, res) => {
